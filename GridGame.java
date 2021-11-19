ad04db1295d7c2b027e5245cdfd7e1e789952d25
Aaditya Vardhan  Narain <aaditya.narain.avna@gmail.com>
2021-11-19 17:08:21 +0530

    Add files via upload
    version 2.0

--- GridGame.java
+++ GridGame.java
@@ -1,36 +1,102 @@
+package practice;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
 public class GridGame  extends JFrame implements ActionListener
 {
     int x,y;
     JButton jb[][];
-    JPanel jp;
+    JMenuBar jmb;
+    JPanel jp=new JPanel();;
     int pos=15;
-    GridGame()// To initialize variable and create a random board to play on
+    Timer t;
+    JLabel jl;
+    ArrayList<Integer> ar;
+    LabelTimer lt;
+    GridGame()
     {
         x=3;
         y=3;
-        setName("GridGame");
+        setName("GridGame");//setting the frame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
-        setSize(new Dimension(400,400));
+        setSize(new Dimension(415,528));
+        setLayout(null);
         
-        JPanel jp=new JPanel();
+        jmb=new JMenuBar();//setting the menubar
+        jmb.setBounds(0, 0, 415, 41);
+        jmb.setBackground(new Color(41,64,82));
+        jmb.setLayout(null);
+        
+        JButton restart=new JButton("RESTART");
+        restart.setForeground(new Color(248,228,204));
+        restart.setBackground(new Color(68,114,148));
+        restart.addActionListener((ActionEvent ae) -> {setNum(jb,ar,jp);
+        });
+        restart.setBounds(10,5,120,25);
+        jmb.add(restart);
+        
+        t=new Timer(true);//to set the timer
+        new Thread(t).start();
+        
+        jl=new JLabel(t.min+" : "+t.sec);//to display the timer
+        jl.setForeground(new Color(244,214,188));
+        jl.setFont(new Font("Ariel",Font.BOLD,20));
+        //jl.setBorder(BorderFactory.createLineBorder(Color.black));
+        jl.setBounds(330, 5,80,30);
+        jmb.add(jl);
+        
+        lt=new LabelTimer(jl,t);
+        new Thread(lt).start();
+             
+        //setting the panel
         jp.setLayout(new GridLayout(4,4,0,0));
+        jp.setBounds(0,40,400,450);
+        jp.setBackground(new Color(244,214,188));
         
-        ArrayList<Integer> ar=new ArrayList();
+        ar=new ArrayList();//creating a random list of numbers
         for(int i=1;i<16;i++)
         {
             ar.add(i);
         }
-       Collections.shuffle(ar);
-        
         jb=new JButton[4][4];
-        
-        for(int i=0;i<4;i++)
+        setNum(jb,ar,jp);
+        add(jmb);
+        add(jp);
+        setVisible(true);
+    }
+    public JButton button(int num)
+    {
+        JButton jb=new JButton(Integer.toString(num));
+        jb.setSize(new Dimension(100,100));
+        jb.setBackground(new Color(21,52,80));
+        jb.setForeground(new Color(248,228,204));
+        jb.setFont(new Font("Ariel",Font.BOLD,40));
+        jb.addActionListener(this);
+        jb.setBorder(BorderFactory.createLineBorder(Color.black));
+        return jb;
+    }
+    public JButton buttonEmpty()
+    {
+        JButton jb=new JButton( );
+        jb.setSize(new Dimension(100,100));
+        jb.setBackground(Color.WHITE);
+        jb.addActionListener(this);
+        return jb;
+    }
+    public void setNum(JButton[][] jb, ArrayList<Integer> ar,JPanel jp)
+    {
+        Collections.shuffle(ar);
+        jp.removeAll();
+        t.min=0;
+        t.sec=0;
+        x=3;
+        y=3;
+        for(int i=0;i<4;i++)//creating buttons
         {
             for(int j=0;j<4;j++)
             {
@@ -49,29 +115,8 @@ public class GridGame  extends JFrame implements ActionListener
                 }
             }
         }
-        
-        add(jp);
-        setVisible(true);
     }
-    public JButton button(int num)// code for clikable button
-    {
-        JButton jb=new JButton(Integer.toString(num));
-        jb.setSize(new Dimension(100,100));
-        jb.setBackground(new Color(21,52,80));
-        jb.setForeground(new Color(248,228,204));
-        jb.setFont(new Font("Ariel",Font.BOLD,40));
-        jb.addActionListener(this);
-        return jb;
-    }
-    public JButton buttonEmpty()//code for empty button
-    {
-        JButton jb=new JButton( );
-        jb.setSize(new Dimension(100,100));
-        jb.setBackground(Color.WHITE);
-        jb.addActionListener(this);
-        return jb;
-    }
-    public void actionPerformed(ActionEvent ae)//to determine the action performed on pressing the buttons
+    public void actionPerformed(ActionEvent ae)
     {
         JButton jt=(JButton)ae.getSource();
         String num=jt.getLabel();
@@ -82,7 +127,9 @@ public class GridGame  extends JFrame implements ActionListener
         Point emptyp=empty.getLocation();
         //need to make cases for all 4 sides
         //this is for left
-        if(emptyp.x-p.x<100 && emptyp.x>p.x && p.y==emptyp.y)//p=210,empty=300
+        System.out.println("SOURCE:"+p.x+":"+p.y);
+        System.out.println("EMPTY:"+emptyp.x+":"+emptyp.y);
+        if(emptyp.x-p.x<=100 && emptyp.x>p.x && p.y==emptyp.y)//p=210,empty=300
         {
             jt.setText(num2);
             jt.setBackground(Color.WHITE);
@@ -91,11 +138,12 @@ public class GridGame  extends JFrame implements ActionListener
             empty.setBackground(new Color(21,52,80));
             empty.setForeground(new Color(248,228,204));
             empty.setFont(new Font("Ariel",Font.BOLD,40));
+            empty.setBorder(BorderFactory.createLineBorder(Color.black));
             empty.addActionListener(this);
             --this.y;
         }
         //for right
-        if(p.x-emptyp.x<100 && p.x>emptyp.x && p.y==emptyp.y)
+        if(p.x-emptyp.x<=100 && p.x>emptyp.x && p.y==emptyp.y)
         {
             jt.setText(num2);
             jt.setBackground(Color.WHITE);
@@ -105,10 +153,11 @@ public class GridGame  extends JFrame implements ActionListener
             empty.setForeground(new Color(248,228,204));
             empty.setFont(new Font("Ariel",Font.BOLD,40));
             empty.addActionListener(this);
+            empty.setBorder(BorderFactory.createLineBorder(Color.black));
             ++this.y;
         }
         //for up
-        if(emptyp.y-p.y<100 && emptyp.y>p.y && p.x==emptyp.x)//p=210,empty=300
+        if(emptyp.y-p.y<=112 && emptyp.y>p.y && p.x==emptyp.x)//p=210,empty=300
         {
             jt.setText(num2);
             jt.setBackground(Color.WHITE);
@@ -118,10 +167,11 @@ public class GridGame  extends JFrame implements ActionListener
             empty.setForeground(new Color(248,228,204));
             empty.setFont(new Font("Ariel",Font.BOLD,40));
             empty.addActionListener(this);
+            empty.setBorder(BorderFactory.createLineBorder(Color.black));
             --this.x;
         }
         //for down
-         if(p.y-emptyp.y<100 && p.y>emptyp.y && p.x==emptyp.x)
+         if(p.y-emptyp.y<=112 && p.y>emptyp.y && p.x==emptyp.x)
         {
             jt.setText(num2);
             jt.setBackground(Color.WHITE);
@@ -131,14 +181,14 @@ public class GridGame  extends JFrame implements ActionListener
             empty.setForeground(new Color(248,228,204));
             empty.setFont(new Font("Ariel",Font.BOLD,40));
             empty.addActionListener(this);
+            empty.setBorder(BorderFactory.createLineBorder(Color.black));
             ++this.x;
         }
         //to check if the game is finished or not
         if(isSolved())
         {
             JOptionPane.showMessageDialog(null, "You Win The Game.");
-            setVisible(false);
-            new GridGame();
+            setNum(jb,ar,jp);
         }
     }
     public boolean isSolved()
@@ -151,6 +201,8 @@ public class GridGame  extends JFrame implements ActionListener
             {
                 if(i==3 && j==3)
                 {
+                    t.min=0;
+                    t.sec=0;
                     return true;
                 }
                 int num=i*4+j+1;
@@ -169,6 +221,8 @@ public class GridGame  extends JFrame implements ActionListener
                 }
             }
         }
+        t.min=0;
+        t.sec=0;
         return true;
     }
     public static void main(String args[])
@@ -179,5 +233,54 @@ public class GridGame  extends JFrame implements ActionListener
                 GridGame gg=new GridGame();
             }
         });
+    }
+}
+class Timer implements Runnable
+{
+    int min,sec;
+    boolean t;
+    Timer(boolean t)
+    {
+        this.t=t;
+        min=0;
+        sec=0;
+    }
+    public void run()
+    {   
+        while(t==true)
+        {
+           try 
+           {
+                Thread.sleep(1000);
+           }
+           catch (InterruptedException ex) 
+            {
+                Logger.getLogger(Timer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            sec++;
+            System.out.println(sec);
+            if(sec>=60)
+            {
+                sec=0;
+                min++;
+            }
+        }   
+    }
+}
+class LabelTimer implements Runnable
+{
+    JLabel jl;
+    Timer t;
+    LabelTimer(JLabel jl,Timer t)
+    {
+        this.jl=jl;
+        this.t=t;
+    }
+    public void run()
+    {
+        while(true)
+        {
+            jl.setText(t.min+" : "+t.sec);
+        }
     }
 }
